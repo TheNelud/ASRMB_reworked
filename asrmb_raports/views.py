b0b0fc971ffc266d24e430d9e393806bb9248104
TheNelud <riwok@bk.ru>
2022-12-31 10:10:59 +0300

    add sar raporst/ tables and forms

--- asrmb_raports/views.py
+++ asrmb_raports/views.py
@@ -19,6 +19,7 @@ def sar(request):
                                             )
 
 def filter_date_sar(request):
+    print (request.path)
     max_date_now = datetime.now().strftime("%Y-%m-%d")
     items_day = Ser_per_day.objects.filter(date_update__contains=request.POST.get('date_update','')).order_by('-id')[:1]
     items_month =Ser_per_month.objects.filter(date_update__contains=request.POST.get('date_update','')).order_by('-id')[:1]
@@ -29,13 +30,16 @@ def filter_date_sar(request):
         delta_day = (just_day - timedelta(days=1)).strftime("%d.%m.%Y")
     else:
         delta_day = "-"
-     
-    return render(request, 'sar.html', context={'items_ser_day' : items_day,
-                                                'items_ser_month' : items_month,
-                                                'max_date_now':max_date_now,
-                                                'delta_day': delta_day
-                                                }) 
 
+    print (request.path)
+    if request.path == "/asrmb_raports/sar/filter/":
+        return render(request, 'sar.html', context={'items_ser_day' : items_day,
+                                                    'items_ser_month' : items_month,
+                                                    'max_date_now':max_date_now,
+                                                    'delta_day': delta_day
+                                                    })
+    else: 
+        pass
 
 
 def sar_edit(request):
