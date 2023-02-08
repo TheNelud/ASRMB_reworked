aed057fa576e70a56e93f451ca11214afb8b3e5e
TheNelud <riwok@bk.ru>
2023-02-08 15:07:18 +0300

    на 8 приложении работают расчет

--- static/js/oks/oks_calculated_p8.js
+++ static/js/oks/oks_calculated_p8.js
@@ -1,48 +1,35 @@
+//for p8
 let in_arr_c1506 = document.querySelectorAll('.js-sum-c1506');
 let in_arr_c1641 = document.querySelectorAll('.js-sum-c1641');
 let in_arr_average_values = document.querySelectorAll('.js-average_value');
 
-let in_sum_c1506 = document.getElementsByClassName('js-sum-c1506-result');
-let in_sum_c1641 = document.getElementsByClassName('js-sum-c1641-result');
-let in_sum_avarage_value = document.getElementsByClassName('js-sum-average_value-result');
-
-let numbers_average_value = [];
-let numbers_c1506 = [];
-let numbers_c1641 = [];
-
-for (let i=0; i < in_arr_c1506.length; i++){
-    in_arr_c1506[i].addEventListener('input',function (){
-        numbers_c1506[i]= this.value;
-        update_result();});
+for (let i=0; i<in_arr_c1506.length-1; i++){
+     in_arr_c1506[i].addEventListener('input', function (){update_result_for_p8();});
 }
 
-for (let i=0; i<in_arr_c1641.length; i++){
-    in_arr_c1641[i].addEventListener('input', function (){
-        numbers_c1641[i] = this.value;
-        update_result();});
-
+for (let i=0; i<in_arr_c1641.length-1; i++){
+     in_arr_c1641[i].addEventListener('input', function (){update_result_for_p8();});
 }
 
-update_result();
 
-function update_result(){
-    in_sum_c1506[0].value=summa_array(numbers_c1506);
-    in_sum_c1641[0].value=summa_array(numbers_c1641);
-    average_value(numbers_c1506,numbers_c1641 );
-    in_sum_avarage_value[0].value = summa_array(numbers_average_value);
+function update_result_for_p8(){
+    summ_c1506();
+    summ_c1641();
+    calculated_average_values();
 }
 
-function average_value(c1506, c1641){
-    for (let i = 0; i < c1506.length; i++){
-        in_arr_average_values[i].value = (Number(c1506[i]) + Number(c1641[i])) / 2;
-        numbers_average_value[i] = in_arr_average_values[i].value;
+function calculated_average_values(){
+    for (let i=0; i<in_arr_average_values.length-1; i++){
+        in_arr_average_values[i].value = (Number(in_arr_c1506[i].value) + Number(in_arr_c1641[i].value)) / 2;
     }
+    in_arr_average_values[11].value = Number(in_arr_average_values[0].value) + Number(in_arr_average_values[1].value) + Number(in_arr_average_values[2].value) + Number(in_arr_average_values[3].value) + Number(in_arr_average_values[4].value) + Number(in_arr_average_values[5].value) + Number(in_arr_average_values[6].value) + Number(in_arr_average_values[7].value) + Number(in_arr_average_values[8].value) + Number(in_arr_average_values[9].value) + Number(in_arr_average_values[10].value);
+    console.log(in_arr_average_values[11])
 }
 
-function summa_array(array){
-    let x = 0;
-    for( let i = 0; i < array.length; i++ ){
-        x += +array[i];
-    }
-    return x;
+function summ_c1506(){
+    in_arr_c1506[11].value = Number(in_arr_c1506[0].value) + Number(in_arr_c1506[1].value) + Number(in_arr_c1506[2].value) + Number(in_arr_c1506[3].value) + Number(in_arr_c1506[4].value) + Number(in_arr_c1506[5].value) + Number(in_arr_c1506[6].value) + Number(in_arr_c1506[7].value) + Number(in_arr_c1506[8].value) + Number(in_arr_c1506[9].value) + Number(in_arr_c1506[10].value);
+}
+
+function summ_c1641() {
+    in_arr_c1641[11].value = Number(in_arr_c1641[0].value) + Number(in_arr_c1641[1].value) + Number(in_arr_c1641[2].value) + Number(in_arr_c1641[3].value) + Number(in_arr_c1641[4].value) + Number(in_arr_c1641[5].value) + Number(in_arr_c1641[6].value) + Number(in_arr_c1641[7].value) + Number(in_arr_c1641[8].value) + Number(in_arr_c1641[9].value) + Number(in_arr_c1641[10].value);
 }
